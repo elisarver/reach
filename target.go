6dcf0ae0498a2a9f5cc8c7c488817560deb502c7
elisarver <eli.sarver@gmail.com>
2015-01-31 19:14:39 -0500

    add documentation

--- target.go
+++ target.go
@@ -11,12 +11,13 @@ type Target struct {
 	*url.URL
 }
 
-// NewTarget generates a Target IFF
+// NewTarget makes a Target from a raw url string
 func NewTarget(rawurl string) (Target, error) {
 	var t Target = Target{&url.URL{}}
 	return t.Parse(rawurl)
 }
 
+// Parse makes a Target from a reference Target
 func (t Target) Parse(rawurl string) (Target, error) {
 	u, err := t.URL.Parse(rawurl)
 	return Target{u}, err
