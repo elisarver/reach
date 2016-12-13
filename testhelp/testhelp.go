b2c1ef3c4bbe3bb5969ea8d5f38b735c172d63ef
elisarver <eli.sarver@gmail.com>
2016-12-12 21:37:26 -0500

    documentation

--- testhelp/testhelp.go
+++ testhelp/testhelp.go
@@ -1,3 +1,4 @@
+// Package testhelp are re-usable test helpers for all modules in reach. 
 package testhelp
 
 import (
@@ -6,14 +7,18 @@ import (
 	"testing"
 )
 
+// Reporter is this package's name for the standard Formatter.
 type Reporter func(message string, values ...interface{})
 
+// Errmsg returns a function that works like fmt.Errorf, but logs the test information
+// and runs it through t's Errorf.
 func Errmsg(t *testing.T, instance string) func(message string, values ...interface{}) {
 	return func(message string, values ...interface{}) {
 		t.Errorf(fmt.Sprintf("%s: %s", instance, message), values...)
 	}
 }
 
+// NewURL creates a new url, and fails the test if it's invalid.
 func NewURL(t *testing.T, u string) *url.URL {
 	url, err := url.Parse(u)
 	if err != nil {
