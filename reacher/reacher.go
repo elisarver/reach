0bc8b625e86e09d5c3d790d8a8eebc06ca9f78eb
Elijah Sarver <esarver@mcclatchyinteractive.com>
2017-02-02 10:27:23 -0500

    Add reparenting functionality to reacher
    - on each Description, if the attribute is acceptable
    - and the value doesn't start with javascript
    - parse the (possible) url as a url relative to the Location context

--- reacher/reacher.go
+++ reacher/reacher.go
@@ -3,6 +3,9 @@ package reacher
 import (
 	"github.com/elisarver/reach/tag"
 	"github.com/elisarver/reach/target"
+	"strings"
+	"fmt"
+	"os"
 	"github.com/elisarver/reach/collections"
 )
 
@@ -20,6 +23,27 @@ var Config = config{
 // URLAttrs represents all attributes that have a URL-like as a value.
 var URLAttrs = collections.Set{"href": nil, "link": nil, "src": nil}
 
+func mp(values []string, fn func(string) (target.Location, error)) []string {
+	for i, v := range values {
+		if !strings.HasPrefix(v, "javascript") {
+			r, e := fn(v)
+			if e != nil {
+				fmt.Fprintf(os.Stderr, "junk value failed parse: %s", v)
+				continue
+			}
+			nv := r.URL.String()
+			if nv != v && nv != "" {
+				values[i] = nv
+			}
+		}
+	}
+	return values
+}
+
+// ReachTargets ranges over locations, and applies the descriptions to each document,
+// in an attempt to extract values out of them. If the global Reparent config option
+// is set, It also applies the URL re-parenting of relative paths to the values,
+// generating more canonical site-oriented urls.
 func ReachTargets(ls []target.Location, ds []*tag.Description) ([]string, error) {
 	var output []string
 	for _, l := range ls {
@@ -28,7 +52,11 @@ func ReachTargets(ls []target.Location, ds []*tag.Description) ([]string, error)
 			return []string{}, err
 		}
 		for _, d := range ds {
-			output = append(output, tag.SelectMap(resp, d)...)
+			var values = tag.SelectMap(resp, d)
+			if Config.Reparent && URLAttrs.Contains(d.Attribute) {
+				values = mp(values, l.Parse)
+			}
+			output = append(output, values...)
 		}
 	}
 	return output, nil
