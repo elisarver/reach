9a854a42b6807ab5945e1d795caa563e428ddbab
Eli Sarver <elis@metrumrg.com>
2021-09-27 12:45:31 -0400

    fix deprecated function use.

--- document/retriever.go
+++ document/retriever.go
@@ -1,6 +1,10 @@
 package document
 
-import "github.com/PuerkitoBio/goquery"
+import (
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
 
 // retriever is a function that implements the same interface as goquery
 type retriever func(location string) (*goquery.Document, error)
@@ -11,9 +15,19 @@ type documentFn func(string) (*goquery.Document, error)
 // genRetrieve generates a new retriever function, useful for testing.
 func genRetrieve(fn documentFn) retriever {
 	if fn == nil {
-		fn = goquery.NewDocument
+		fn = document
 	}
 	return func(location string) (*goquery.Document, error) {
 		return fn(location)
 	}
+}
+
+func document(url string) (*goquery.Document, error) {
+	res, e := http.Get(url)
+	if e != nil {
+		return nil, e
+	}
+	defer res.Body.Close()
+
+	return goquery.NewDocumentFromReader(res.Body)
 }
